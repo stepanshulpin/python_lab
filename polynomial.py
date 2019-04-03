63cca17b417c3fbb80e387828ccf27a0bbb31770
stepanshulpin <steve13.12@yandex.ru>
2019-04-03 22:17:08 +0300

    implemented all

--- polynomial.py
+++ polynomial.py
@@ -1,25 +1,31 @@
 class Polynomial(object):
     def __init__(self, coefficients):
-        i = 0
-        while i < len(coefficients):
-            if coefficients[i]==0:
-                coefficients.pop(0)
-                i-=1
-            elif coefficients[i]!=0:
-                break
-            i+=1
-        self.coefficients = coefficients
+        if isinstance(coefficients, (list,)):
+            i = 0
+            while i < len(coefficients):
+                if coefficients[i] == 0:
+                    coefficients.pop(0)
+                    i -= 1
+                elif coefficients[i] != 0:
+                    break
+                i += 1
+            if len(coefficients) != 0:
+                self.coefficients = coefficients
+            else:
+                raise ValueError
+        else:
+            raise TypeError
 
     def __str__(self):
-        res=""
-        i=0
+        res = ""
+        i = 0
         for c in reversed(self.coefficients):
-            if c!=0:
+            if c != 0:
                 space = " " if len(res) > 0 else ""
-                if i==0:
+                if i == 0:
                     var_in_degree = "" if abs(c) != 1 else "1"
-                elif i==1:
-                    var_in_degree = "x" +  space
+                elif i == 1:
+                    var_in_degree = "x" + space
                 else:
                     var_in_degree = "x^" + str(i) + space
 
@@ -27,11 +33,11 @@ class Polynomial(object):
                     res = str(abs(c)) + var_in_degree + res
                 else:
                     res = var_in_degree + res
-                if i!=len(self.coefficients)-1:
+                if i != len(self.coefficients) - 1:
                     res = sign(c) + " " + res
-                elif c<0:
+                elif c < 0:
                     res = sign(c) + res
-            i+=1
+            i += 1
         return res
 
     def __repr__(self):
@@ -39,52 +45,99 @@ class Polynomial(object):
 
     def __add__(self, other):
         if isinstance(other, Polynomial):
-            c1=self.coefficients
-            c2=other.coefficients
-            l=max(len(c1),len(c2))
-            while len(c1)<l: c1.insert(0,0)
-            while len(c2)<l: c2.insert(0,0)
-            coefficients = list(map(lambda x,y:x+y, self.coefficients, other.coefficients))
-            return Polynomial(list(coefficients))
+            c1 = self.coefficients
+            c2 = other.coefficients
+            l = max(len(c1), len(c2))
+            while len(c1) < l: c1.insert(0, 0)
+            while len(c2) < l: c2.insert(0, 0)
+            coefficients = list(map(lambda x, y: x + y, self.coefficients, other.coefficients))
+            return Polynomial(coefficients)
         if isinstance(other, int):
-            c=self.coefficients
-            c[len(c)-1]+=other
+            c = self.coefficients
+            c[len(c) - 1] += other
             return Polynomial(list(c))
+        raise TypeError
 
     def __radd__(self, other):
         if isinstance(other, int):
             return self + other
+        raise TypeError
 
     def __sub__(self, other):
         if isinstance(other, Polynomial):
-            c1=self.coefficients
-            c2=other.coefficients
-            l=max(len(c1),len(c2))
-            while len(c1)<l: c1.insert(0,0)
-            while len(c2)<l: c2.insert(0,0)
-            coefficients = list(map(lambda x,y:x-y, self.coefficients, other.coefficients))
-            return Polynomial(list(coefficients))
+            c1 = self.coefficients
+            c2 = other.coefficients
+            l = max(len(c1), len(c2))
+            while len(c1) < l: c1.insert(0, 0)
+            while len(c2) < l: c2.insert(0, 0)
+            coefficients = list(map(lambda x, y: x - y, self.coefficients, other.coefficients))
+            return Polynomial(coefficients)
         if isinstance(other, int):
-            c=self.coefficients
-            c[len(c)-1]-=other
+            c = self.coefficients
+            c[len(c) - 1] -= other
             return Polynomial(list(c))
+        raise TypeError
 
     def __rsub__(self, other):
         if isinstance(other, int):
-            c=list(map(lambda x:(-1)*x,self.coefficients))
-            free_member = other+c[len(c)-1]
-            c[len(c)-1]=free_member
+            c = list(map(lambda x: (-1) * x, self.coefficients))
+            free_member = other + c[len(c) - 1]
+            c[len(c) - 1] = free_member
             return Polynomial(c)
+        raise TypeError
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            c = [0] * (len(self.coefficients) + len(other.coefficients) - 1)
+            for i1, c1 in enumerate(self.coefficients):
+                for i2, c2 in enumerate(other.coefficients):
+                    c[i1 + i2] += c1 * c2
+            return Polynomial(c)
+        if isinstance(other, int):
+            return Polynomial(list(map(lambda x: other * x, self.coefficients)))
+        raise TypeError
+
+    def __rmul__(self, other):
+        if isinstance(other, int):
+            return self * other
+        raise TypeError
 
     def __eq__(self, other):
-        return self.coefficients == other.coefficients
+        if isinstance(other, Polynomial):
+            return self.coefficients == other.coefficients
+        raise TypeError
 
+    def __lt__(self, other):
+        if isinstance(other, Polynomial):
+            if len(self.coefficients) == len(other.coefficients):
+                return self.coefficients < other.coefficients
+            else:
+                return len(self.coefficients) < len(other.coefficients)
+        raise TypeError
 
+    def __le__(self, other):
+        if isinstance(other, Polynomial):
+            return self.__lt__(other) or self == other
+        raise TypeError
 
+    def __ne__(self, other):
+        if isinstance(other, Polynomial):
+            return self.coefficients != other.coefficients
+        raise TypeError
+
+    def __gt__(self, other):
+        if isinstance(other, Polynomial):
+            return not self.__le__(other)
+        raise TypeError
+
+    def __ge__(self, other):
+        if isinstance(other, Polynomial):
+            return not self.__lt__(other)
+        raise TypeError
 
 
 def sign(i):
-    if i>0:
+    if i > 0:
         return "+"
-    elif i<0:
+    elif i < 0:
         return "-"
--- polynomial_unit_tests.py
+++ polynomial_unit_tests.py
@@ -1,20 +1,25 @@
 import unittest
 from polynomial import Polynomial
 
+
 class PolynomialUnitTest(unittest.TestCase):
 
     def setUp(self):
-        self.p1 = Polynomial([3,-2,0,-2])
-        self.p2 = Polynomial([-1,1,3,0])
-        self.p3 = Polynomial([0,-2,0,0])
+        self.p1 = Polynomial([3, -2, 0, -2])
+        self.p2 = Polynomial([-1, 1, 3, 0])
+        self.p3 = Polynomial([0, -2, 0, 0])
 
-    #TODO: добавить проверку на выбрасывание исключений для невалидных данных
+    def test_init_value_error(self):
+        self.assertRaises(ValueError, Polynomial, [0, 0, 0])
+
+    def test_init_type_error(self):
+        self.assertRaises(TypeError, Polynomial, "Hello")
 
     def test_init_p1(self):
-        self.assertEqual([3,-2,0,-2],self.p1.coefficients)
+        self.assertEqual([3, -2, 0, -2], self.p1.coefficients)
 
     def test_init_p3(self):
-        self.assertEqual([-2,0,0],self.p3.coefficients)
+        self.assertEqual([-2, 0, 0], self.p3.coefficients)
 
     def test_str_p1(self):
         self.assertEqual("3x^3 - 2x^2 - 2", str(self.p1))
@@ -35,10 +40,101 @@ class PolynomialUnitTest(unittest.TestCase):
         self.assertEqual("Polynomial([-2, 0, 0])", repr(self.p3))
 
     def test_add_p1_p2(self):
-        self.assertEqual(Polynomial([2,-1,3,-2]),self.p1+self.p2)
+        self.assertEqual(Polynomial([2, -1, 3, -2]), self.p1 + self.p2)
 
     def test_add_p2_p3(self):
-        self.assertEqual(Polynomial([-1,-1,3,0]),self.p2+self.p3)
+        self.assertEqual(Polynomial([-1, -1, 3, 0]), self.p2 + self.p3)
 
     def test_add_p1_p3(self):
-        self.assertEqual(Polynomial([3,-4,0,-2]),self.p1+self.p3)
+        self.assertEqual(Polynomial([3, -4, 0, -2]), self.p1 + self.p3)
+
+    def test_add_p1_const(self):
+        self.assertEqual(Polynomial([3, -2, 0, 1]), self.p1 + 3)
+
+    def test_add_const_p1(self):
+        self.assertEqual(Polynomial([3, -2, 0, 1]), 3 + self.p1)
+
+    def test_add_type_error(self):
+        self.assertRaises(TypeError, self.p1.__add__, "Hello")
+
+    def test_sub_p1_p2(self):
+        self.assertEqual(Polynomial([4, -3, -3, -2]), self.p1 - self.p2)
+
+    def test_sub_p2_p3(self):
+        self.assertEqual(Polynomial([-1, 3, 3, 0]), self.p2 - self.p3)
+
+    def test_sub_p1_p3(self):
+        self.assertEqual(Polynomial([3, 0, 0, -2]), self.p1 - self.p3)
+
+    def test_sub_p1_const(self):
+        self.assertEqual(Polynomial([3, -2, 0, -5]), self.p1 - 3)
+
+    def test_sub_const_p1(self):
+        self.assertEqual(Polynomial([-3, 2, 0, 5]), 3 - self.p1)
+
+    def test_sub_type_error(self):
+        self.assertRaises(TypeError, self.p1.__sub__, "Hello")
+
+    def test_mul_p1_p2(self):
+        self.assertEqual(Polynomial([-3, 5, 7, -4, -2, -6, 0]), self.p1 * self.p2)
+
+    def test_mul_p2_p3(self):
+        self.assertEqual(Polynomial([2, -2, -6, 0, 0, 0]), self.p2 * self.p3)
+
+    def test_mul_p1_p3(self):
+        self.assertEqual(Polynomial([-6, 4, 0, 4, 0, 0]), self.p1 * self.p3)
+
+    def test_mul_p1_const(self):
+        self.assertEqual(Polynomial([9, -6, 0, -6]), self.p1 * 3)
+
+    def test_mul_const_p1(self):
+        self.assertEqual(Polynomial([9, -6, 0, -6]), 3 * self.p1)
+
+    def test_mul_type_error(self):
+        self.assertRaises(TypeError, self.p1.__mul__, "Hello")
+
+    def test_eq_p1(self):
+        self.assertTrue(self.p1 == Polynomial([3, -2, 0, -2]))
+
+    def test_eq_type_error(self):
+        self.assertRaises(TypeError, self.p1.__eq__, "Hello")
+
+    def test_ne_p1(self):
+        self.assertTrue(self.p1 != self.p2)
+
+    def test_ne_type_error(self):
+        self.assertRaises(TypeError, self.p1.__ne__, "Hello")
+
+    def test_lt_p1_p2(self):
+        self.assertFalse(self.p1 < self.p2)
+
+    def test_lt_p2_p3(self):
+        self.assertFalse(self.p2 < self.p3)
+
+    def test_lt_type_error(self):
+        self.assertRaises(TypeError, self.p1.__lt__, "Hello")
+
+    def test_le_p3_p1(self):
+        self.assertTrue(self.p3 <= self.p1)
+
+    def test_le_p1(self):
+        self.assertTrue(self.p1 <= Polynomial([3, -2, 0, -2]))
+
+    def test_le_type_error(self):
+        self.assertRaises(TypeError, self.p1.__le__, "Hello")
+
+    def test_gt_p2_p3(self):
+        self.assertTrue(self.p2 > self.p3)
+
+    def test_gt_type_error(self):
+        self.assertRaises(TypeError, self.p1.__gt__, "Hello")
+
+    def test_ge_p3_p1(self):
+        self.assertFalse(self.p3 >= self.p1)
+
+    def test_ge_type_error(self):
+        self.assertRaises(TypeError, self.p1.__ge__, "Hello")
+
+
+if __name__ == '__main__':
+    unittest.main()
